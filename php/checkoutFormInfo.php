93624bb7415a6bc4fbee671b90a3e4a974f140d3
tim <timdesmet00@gmail.com>
2023-10-25 12:23:28 +0200

    creation et premier jet du fichier checkoutFormInfo

---
+++ php/checkoutFormInfo.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<form method="POST" action="" class="form">
+    <label for="firstName">Firstname:</label>
+    <input type="text" name="firstName" id="firstName" minlength="3" maxlength="20" required>
+
+    <label for="lastName">Lastname:</label>
+    <input type="text" name="lastName" id="lastName" minlength="3" maxlength="20" required>
+
+    <label for="email">Email:</label>
+    <input type="email" name="email" id="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required>
+
+    <label for="address">Address:</label>
+    <input type="text" name="address" id="address" minlength="3" required>
+        
+    <label for="city">City:</label>
+    <input type="text" name="city" id="city" minlength="3" required>
+
+    <label for="zipCode">Zip Code:</label>
+    <input type="number" name="zipCode" id="zipCode" minlength="3" required>
+
+    <label for="country">Country:</label>
+    <input type="text" name="country" id="country" minlength="3" required>
+
+    <input type="submit" name="submit" value="Submit">
+</form>
