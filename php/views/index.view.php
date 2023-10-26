930daa2c03d6cdf3f81ab4e84d79366d3942f5d7
Pierremarien <pierremarien29@gmail.com>
2023-10-26 15:53:51 +0200

    wip

--- php/views/index.view.php
+++ php/views/index.view.php
@@ -7,19 +7,19 @@ require "partials/head.php";
         <?php
         require "partials/nav.php";
         ?>
-        <div>
+        <div class="title">
             <h1>Shoe the right <span class="blue">one</span></h1>
-            <button>See our store</button>
+            <button class="btn btn--store">See our store</button>
         </div>
-        <div>
+        <div class="best_shoe">
             <figure>
                 <img src="../../assets/img/shoe_one.png" alt="our best shoe">
             </figure>
-            <span>NIKE</span>       
+            <span class="filigrane">NIKE</span>       
         </div>
     </header>
     <main>
-        <section>
+        <section class="latest_products">
             <h3><span class="blue">Our</span> last products</h3>
             
             <?php
@@ -32,10 +32,10 @@ require "partials/head.php";
                 foreach ($products as $product) {
                     
                     echo '<div class="product">';
-                    echo '<p>' . $product['product'] . '</p>';
-                    echo '<img src="../../' . $product['image_url'] . '" alt="' . $product['product'] . '">';
-                    echo '<p>Price: $' . $product['price'] . '</p>';
-                    echo "<a href=\"../add-to-session.php?id={$product['id']}\">Add to cart</a>";
+                    echo '<p class="product_name">' . $product['product'] . '</p>';
+                    echo '<figure> <img src="../../' . $product['image_url'] . '" alt="' . $product['product'] . '"> </figure>';
+                    echo '<p class="product_price">Price: $' . $product['price'] . '</p>';
+                    echo "<a class='btn' href=\"../add-to-session.php?id={$product['id']}\">Add to cart</a>";
                     echo '</div>';
                 }
             } else {
@@ -44,29 +44,29 @@ require "partials/head.php";
             ?>
 
         </section>
-        <section>
+        <section class="pub">
             <figure>
                 <img src="../../assets/img/shoe_two.png" alt="our second best shoe">
             </figure>
             <h2>We provide you the <span class="blue">best</span> quality</h2>
             <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae mollitia cum saepe consequatur iste odio magni adipisci odit nulla, quae omnis a?"</p>
         </section>
-        <section>
-            <div>
+        <section class="partners">
+            <div class="partner">
                 <figure>
                     <img src="../../assets/img/image-emily.jpg" alt="this is emily">
                 </figure>
                 <h4>Emily from xyz</h4>
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error aut harum veniam dolores vitae!</p>
             </div>
-            <div>
+            <div class="partner">
                 <figure>
                     <img src="../../assets/img/image-thomas.jpg" alt="this is thomas">
                 </figure>
                 <h4>Thomas from corporate</h4>
                 <p>"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error aut harum veniam dolores vitae!"</p>
             </div>
-            <div>
+            <div class="partner">
                 <figure>
                     <img src="../../assets/img/image-jennie.jpg" alt="this is jennie">
                 </figure>
