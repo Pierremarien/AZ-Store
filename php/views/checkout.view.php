d6e8088adf73fc5babfd8a76d34c68d806a8fca8
tim <timdesmet00@gmail.com>
2023-10-26 15:18:23 +0200

    divers correction

--- php/views/checkout.view.php
+++ php/views/checkout.view.php
@@ -33,6 +33,9 @@
     </div>
 </form>
 <?php
+require "partials/head.php";
+
+require "partials/nav.php";
 
 // var_dump($_POST);
 function sanitizeValidation ($_post) {
@@ -98,8 +101,45 @@ function sanitizeValidation ($_post) {
 if(array_key_exists('submit', $_POST)) { 
     sanitizeValidation($_POST);
 } 
+?>
 
+<form method="POST" action="" class="form">
+    <div>
+        <label for="firstName">Firstname:</label>
+        <input type="text" name="firstName" id="firstName" minlength="3" maxlength="20" required>
+    
+        <label for="lastName">Lastname:</label>
+        <input type="text" name="lastName" id="lastName" minlength="3" maxlength="20" required>
+    </div>
 
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" required>
+    
+        <label for="address">Address:</label>
+        <input type="text" name="address" id="address" minlength="3" required>
+    </div>
 
+    <div>    
+        <label for="city">City:</label>
+        <input type="text" name="city" id="city" minlength="3" maxlength="20" required>
+
+        <label for="zipCode">Zip Code:</label>
+        <input type="number" name="zipCode" id="zipCode" minlength="3" maxlength="20" required>
+    </div>
+
+    <div>
+        <label for="country">Country:</label>
+        <input type="text" name="country" id="country" minlength="3" maxlength="20" required>
+    </div>
+    
+    <div>
+        <input type="submit" name="submit" value="Submit">
+    </div>
+</form>
+
+<?php
+
+require "partials/footer.php";
 ?>
 
--- php/views/partials/deleteBtn.php
+++ php/views/partials/deleteBtn.php
@@ -1,6 +1,6 @@
 <?php
 if(array_key_exists('delete', $_POST)) { 
-    array_filter($_SESSION['cart'], function($item) {
+    array_filter($_SESSION['article'], function($item) {
         if($item['id'] !== $_POST['delete']) {
             return $item;
         }
